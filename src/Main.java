2907f6f43a26ea0f32304ff075abf120bedfcccd
Thang0226 <thang.nd0226@gmail.com>
2024-10-14 11:32:30 +0700

    clean code

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,16 @@ public class Main {
 			numbers[i] = Double.parseDouble(tokens[i]);
 		}
 
-		QuadraticEquation equation = new QuadraticEquation(numbers[0], numbers[1], numbers[2]);
+		QuadraticEquation equation = new QuadraticEquation(numbers[0],
+				numbers[1], numbers[2]);
 		if (equation.getDiscriminant() < 0) {
 			System.out.println("The equation has no real roots");
 		} else if (equation.getDiscriminant() == 0) {
-			System.out.println("The equation has one root: " + equation.getRoot1());
+			System.out.println("The equation has one root: "
+					+ equation.getRoot1());
 		} else {
-			System.out.println("The equation has two roots: " + equation.getRoot1() + " and " + equation.getRoot2());
+			System.out.println("The equation has two roots: "
+					+ equation.getRoot1() + " and " + equation.getRoot2());
 		}
 	}
 }
